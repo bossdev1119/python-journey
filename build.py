4eda760225b583b82588117187ceaac0966f14da
bossdev1119 <sundram.vk18@gmail.com>
2026-01-17 15:12:02 +0530

    new file added  , html file deleted

---
+++ build.py
@@ -0,0 +1,13 @@
+# name = input("Enter your name: ")
+# day = input("birth day: ")
+# month = input("birth month: ")
+# year = input("birth year: ")
+
+# print("hello ! "+ name + " you are " + str ( (2025 - int(year))) + " years old")
+
+# l= input("do you love someone ? (yes/no): ")
+# if l== "yes":
+#     print("woww ! tum dono ek dusre ke liye bane ho")
+#     print("best wishes for your life together")
+# else:
+#     print("no worries ! you will find someone special one day")
