1411a8c162d078d2f50e0cd4e7053695dba3a6b8
bossdev1119 <sundram.vk18@gmail.com>
2026-01-17 16:30:41 +0530

    day2 : IF ELSE PROGRAM

--- build.py
+++ build.py
@@ -11,3 +11,23 @@
 #     print("best wishes for your life together")
 # else:
 #     print("no worries ! you will find someone special one day")
+
+
+# -------Day 1-------------------array --------------------
+
+# import array as arr
+# a= arr.array ('i',[1,2,4,5,7])
+
+# for i in a :
+#     print(i)
+# print(a[2])
+# a.append(8)
+# a.remove(4)
+# print(a)
+
+# li= a.tolist()
+# print ("The new list created is : ",end="")
+# for i in range (0,len(li)):
+#     print (li[i],end=" ")
+
+# -------Day 2-------------------loops --------------------
---
+++ day2.py
@@ -0,0 +1,22 @@
+# Check if a number is even or odd and positive or negative
+
+# i = int(input("Enter a number: "))
+# if i >= 0 and i % 2 == 0:
+#     print("The number is Even and Positive")
+# elif i >= 0 and i % 2 != 0:
+#     print("The number is Odd and Positive") 
+# else:
+#     print("The number is Negative")   
+
+
+# Grade calculation based on marks-----------------------
+
+marks = int(input("Enter your marks: "))
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Grade fail")
