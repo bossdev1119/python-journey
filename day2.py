a6e9fcd6634da20245fd41b52ef7473d94d914fd
bossdev1119 <sundram.vk18@gmail.com>
2026-01-18 14:52:18 +0530

    Add grade logic to day2.py

--- day2.py
+++ day2.py
@@ -11,12 +11,13 @@
 
 # Grade calculation based on marks-----------------------
 
-marks = int(input("Enter your marks: "))
-if marks >= 90:
-    print("Grade A")
-elif marks >= 75:
-    print("Grade B")
-elif marks >= 60:
-    print("Grade C")
-else:
-    print("Grade fail")
+# marks = int(input("Enter your marks: "))
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 75:
+#     print("Grade B")
+# elif marks >= 60:
+#     print("Grade C")
+# else:
+#     print("Grade fail")
+
