d1a6000e0da9930f5d826e38584ca2f477dea1a7
bossdev1119 <sundram.vk18@gmail.com>
2026-01-20 02:55:07 +0530

    learnt about list and dict in python

---
+++ day4.py
@@ -0,0 +1,15 @@
+# -----------list operations----------------
+
+# list1=[]
+# sub= int(input("how many subjects marks you want to enter: "))
+# for i in range(sub):
+#     marks=float (input(f"enter marks for subject {i+1}: "))
+#     list1.append(marks)
+# print("total marks obtained:",sum(list1))
+# print("average marks:",sum(list1)/sub)
+# print("highest marks:", max(list1))
+# print("lowest marks:", min(list1))
+
+
+# -----------dictionary operations----------------
+# this concept is applied in phonebook cli app
